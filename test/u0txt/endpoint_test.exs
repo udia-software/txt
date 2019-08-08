f65ac288568aef9ddb80685b47d34ea0c6e55eea
Alexander Wong <alex@udia.ca>
2019-08-08 16:29:38 -0600

    fixed tests

--- test/u0txt/endpoint_test.exs
+++ test/u0txt/endpoint_test.exs
@@ -4,20 +4,7 @@ defmodule U0txt.EndpointTest do
 
   @opts U0txt.Endpoint.init([])
 
-  test "it returns pong" do
-    # Create a test connection
-    conn = conn(:get, "/ping")
-
-    # Invoke the plug
-    conn = U0txt.Endpoint.call(conn, @opts)
-
-    # Assert the response and status
-    assert conn.state == :sent
-    assert conn.status == 200
-    assert conn.resp_body == "pong!"
-  end
-
-  test "it returns 200 for hello world" do
+  test "it returns 200 for /" do
     # Create a test connection
     conn = conn(:get, "/")
 
@@ -27,17 +14,41 @@ defmodule U0txt.EndpointTest do
     # Assert the response
     assert conn.state == :sent
     assert conn.status == 200
-    assert conn.resp_body == "Hello, world!"
+    assert conn.resp_body == "<!DOCTYPE HTML>\n<html lang=\"en\">\n<head><title>u0txt</title>\n<meta name=\"description\" content=\"u0txt: ommand line pastebin\"/></head>\n<body><pre>            txt.udia.ca\nNAME:\n  u0txt: command line pastebin.\n\nUSAGE:\n  &lt;cmd&gt; | curl -F 'txt=&lt;-' https://txt.udia.ca\n  or upload from web:\n<form action=\"/\" method=\"POST\"><input name=\"web\" type=\"hidden\" value=\"true\">\n<textarea name=\"txt\" cols=\"60\" rows=\"20\"></textarea>\n<br><input type=\"submit\" value=\"Submit\" /></form>\n\nDESCRIPTION\n  I wanted to write my own text pastebin using Elixir/Erlang and Mnesia.\n\nEXAMPLES\n  ~$ cat yourfile | curl -F 'txt=<-' https://txt.udia.ca\n  https://txt.udia.ca/MOJV\n  ~$ firefox https://txt.udia.ca/MOJV\n\n  Add this to your .*rc file:\n\n  alias txt=\"\\\n    sed -r 's/\\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g' \\\n    | curl -F 'txt=<-' https://txt.udia.ca\"\n\n  Now you can pipe directly into txt! Sed removes colours.\n\nSOURCE CODE\n  https://github.com/udia-software/txt\n\nSEE ALSO\n  https://txt.t0.vc/\n</pre></body></html>"
+  end
+
+  test "it handles bad posts" do
+    conn = conn(:post, "/", "badpost")
+    conn = U0txt.Endpoint.call(conn, @opts)
+
+    assert conn.state == :sent
+    assert conn.status == 400
+    assert conn.resp_body == "INVALID REQUEST"
+  end
+
+  test "it pastebins good posts" do
+    conn = conn(:post, "/", %{txt: "hellothere"})
+    conn = U0txt.Endpoint.call(conn, @opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    path = conn.resp_body
+      |> String.replace("\n", "")
+      |> String.replace("https://txt.udia.ca", "")
+    assert String.length(path) == 7
+
+    conn = conn(:get, path)
+    conn = U0txt.Endpoint.call(conn, @opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "hellothere"
   end
 
   test "it returns 404 when no route matches" do
-    # Create a test connection
     conn = conn(:get, "/fail")
-
-    # Invoke the plug
     conn = U0txt.Endpoint.call(conn, @opts)
 
-    # Assert the response
     assert conn.status == 404
   end
 end
